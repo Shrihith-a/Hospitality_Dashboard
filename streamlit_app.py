88987e931529f4a7464c539e743d345a62b1d3be
Shrihith <shrihith93@gmail.com>
2023-03-11 01:09:08 +0530

    EDA for dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,53 +3,30 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-st.set_option('deprecation.showPyplotGlobalUse', False) ## Streamlit prompts an error when i insert my barchart. This removes the error
+from numerize.numerize import numerize
 
-# Initialize a session state variable that tracks the sidebar state (either 'expanded' or 'collapsed').
-if 'sidebar_state' not in st.session_state:
-    st.session_state.sidebar_state = 'expanded'
+st.set_page_config(page_title="Hospitality Atliq Grand Dashboard",
+                   layout='wide',
+                   initial_sidebar_state='expanded'
+                   )
 
-# Streamlit set_page_config method has a 'initial_sidebar_state' argument that controls sidebar state.
-# Basic Page Configurations to to make GUI a little better
-# Set the page title and icon
-st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state,
-                   page_title="Hospitality Dashboard",
-                   page_icon="🎪",
-                   layout="wide"
-                    )
+# Loading Dataframes cache
 
-# Hide the menu button
-# While this menu does contain all sorts of top-notch stuff, you might want to hide it when you deploy your app to users.
-st.markdown(""" <style>
-MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
+@st.cache_data
+def get_data():
+    df = pd.read_csv('Dataset/dim_date.csv') 
+    df['date']= pd.to_datetime(df['date'])
+    return df
+df = get_data()
 
-# Condense the layout
-padding = 0
-st.markdown(f""" <style>
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
+def get_data1():
+    df = pd.read_csv('Dataset/dim_hotels.csv')  
+    return df
+df1 = get_data1()
 
-# Custom color palette
-# add .streamlit folder and add config.toml file
+header_left,header_mid,header_right = st.columns([1,2,1],gap='large')
 
-# Show title and description of the app.
-st.title('Click the sidebar')
-st.sidebar.markdown('This is an example Streamlit app to show how to expand and collapse the sidebar programmatically.')
-
-# Toggle sidebar state between 'expanded' and 'collapsed'.
-if st.button('Click to toggle sidebar state'):
-    st.session_state.sidebar_state = 'collapsed' if st.session_state.sidebar_state == 'expanded' else 'expanded'
-    # Force an app rerun after switching the sidebar state.
-    st.experimental_rerun()
-
-# Adding Header
-st.markdown("<hr/>",unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center;'>Atliq Grands Hotel's Hospitality Data Visualization</h1>", unsafe_allow_html=True)
+with header_mid:
+    st.title('Hospitality Atliq Dashboard')
 
 
